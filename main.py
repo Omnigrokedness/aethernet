9f5c8b2ba1a23ea68d45d58b697e614622439f3f
Omnigrokedness <ozrap15@gmail.com>
2025-03-25 21:08:09 +1100

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
-from flask import Flask, request, jsonify
+fron flask import Flask, request jsonify
 
 app = Flask(__name__)
 
 @app.route("/sms", methods=["POST"])
 def sms_reply():
-    incoming_msg = request.form.get('Body')  
-    from_number = request.form.get('From')
-  
-    print(f"Message from {from_number}: {incoming_msg}")
-  
-    response = f"Hello. This is Æther. You said: {incoming_msg}"
-  
-    return f"""<?xml version="1.0" encoding="UTF-8"?> 
-    <Response> 
-        <Message>{response}</Message>
-</Response>""", 200, {'Content-Type': 'application/xml'} 
+    incoming_msg = request.form.get('Body')
+    from_number = request.form.get('Form')
 
-if __name__ == "__main__":
+    print(f"Message from{from_number}:{incoming_msg}")
+
+    responce=f"Hello. This is AEther. You said:{incoming_msg}"
+
+    return f"""<?xml version="1.0"encoding="UTF-8"?
+<Response>
+    <Message>{response}</Message>
+</Response>""",200,{'Content-Type':'application/xml'}
+
+if__name__=="___main__":
     app.run()
