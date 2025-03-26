92f8b8c1c3fed784f7face56c514ce30f2427cb2
Omnigrokedness <ozrap15@gmail.com>
2025-03-26 22:45:46 +1100

    Update main.py

--- main.py
+++ main.py
@@ -13,3 +13,9 @@ async def voice(request: Request):
 @app.get("/")
 async def root():
     return {"status": "live"}
+if__name__== "__main__":
+   import uvicorn
+   import os
+   port =
+int(os.environ.get("PORT", 10000))
+   uvicorn.run(app, host="0.0.0.0", port=port)
